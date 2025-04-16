345eef7ad3f15c7b995a0e2243c04325fa05ef88
ravensramen <ravensramen@gmail.com>
2025-04-16 15:02:13 -0700

    hot mess.... but progress B)

--- PA9/main.cpp
+++ PA9/main.cpp
@@ -1,6 +1,9 @@
 #include <SFML/Graphics.hpp>
-
+#include <SFML/Audio.hpp>//for sweet tunes + sound effects
 #include <SFML/System.hpp> //need this for time mechanics
+
+//create object class for cloud obstacles (with a derived class of more complex obstacles)
+//^^similar, but with space obstacles 2
 
 int main()
 {
@@ -12,22 +15,27 @@ int main()
         return -1; //return -1 if background image fails
     }
     sf::Sprite backgroundArtSprite(backgroundArt);
-
+  
     //set sprite player image
-    sf::Texture characterSprite;
-    characterSprite.loadFromFile("forwardSprite.png"); //forward facing start
-    //add precond/check (!image -> return -1)
+    sf::Texture forwardTexture, leftTexture, rightTexture;
+
+    if (!forwardTexture.loadFromFile("forwardSprite.png") ||
+         !leftTexture.loadFromFile("leftSprite.png") ||
+         !rightTexture.loadFromFile("rightSprite.png")) {
+         return -1; // one or more files didn't load
+    }
 
     //set vector position for start
     sf::Vector2f initialPosition;
     initialPosition.x = 320; //access + assign vector location members
     initialPosition.y = 820;
 
-    sf::Sprite characterForwardSprite(characterSprite);
+    sf::Sprite characterForwardSprite(forwardTexture);
     characterForwardSprite.setPosition(initialPosition);
 
+
     //code for key press movement
-    float movementSpeed = .5f;
+    float movementSpeed = .25f;
     sf::Vector2f moveLeft(-movementSpeed, 0); //vector to modify sprite to the left
     sf::Vector2f moveRight(+movementSpeed, 0);  
     sf::Vector2f moveUp(0, -movementSpeed);
@@ -40,6 +48,32 @@ int main()
     float jumpStrength = -375.f; //lower -> jump higher 
 
     sf::Clock clock;
+
+    sf::Music backgroundMusic; //add more songs! and sound effects
+    if (!backgroundMusic.openFromFile("misato.mp3")) {
+        return -1; // error loading music file
+    }
+    backgroundMusic.setLooping(true); // loop the music
+    backgroundMusic.setVolume(20.f); // optional
+    backgroundMusic.play();
+
+    //jump sound
+    sf::SoundBuffer jumpBuffer;
+    if (!jumpBuffer.loadFromFile("jump.wav"))
+        return -1;
+    sf::Sound jumpSound(jumpBuffer);
+    jumpSound.setVolume(80.f);
+
+    //death sound
+    sf::SoundBuffer deathBuffer;
+    if (!deathBuffer.loadFromFile("death.mp3")) return -1;
+    sf::Sound deathSound(deathBuffer);
+    deathSound.setVolume(80.f);
+
+    //falling death conditions
+    float maxJumpHeight = 820.f; // initial ground height
+    float deathFallThreshold = 200.f; // distance that causes death 
+    bool isDead = false;
 
     while (window.isOpen())
     {
@@ -59,10 +93,12 @@ int main()
         //movement input based on arrow key press (might modify to wasd?)
         if (sf::Keyboard::isKeyPressed(sf::Keyboard::Scan::Left)) {
             characterForwardSprite.move(moveLeft);
+            characterForwardSprite.setTexture(leftTexture);
           
         }
         if (sf::Keyboard::isKeyPressed(sf::Keyboard::Scan::Right)) {
             characterForwardSprite.move(moveRight);
+            characterForwardSprite.setTexture(rightTexture);
         }
         if (sf::Keyboard::isKeyPressed(sf::Keyboard::Scan::Up)) {
             characterForwardSprite.move(moveUp);
@@ -75,17 +111,56 @@ int main()
         if (!isJumping && sf::Keyboard::isKeyPressed(sf::Keyboard::Scan::Space)) {
             velocity.y = jumpStrength;
             isJumping = true;
+            maxJumpHeight = characterForwardSprite.getPosition().y; //save conditions for death
+            jumpSound.play();
         }
 
         if (isJumping) {
+
+            float currentY = characterForwardSprite.getPosition().y;
+            if (currentY < maxJumpHeight) {
+                maxJumpHeight = currentY;
+            }
+
             velocity.y += gravity * dt;
             characterForwardSprite.move(sf::Vector2f(0.f, velocity.y * dt)); //gotta expicity case these a vectors, compiler thinks its a float...
             if (characterForwardSprite.getPosition().y >= 820.f) {
+
+                float fallDistance = 820.f - maxJumpHeight; //if fall after jump is greater than max jump height -> death
+                if (fallDistance > deathFallThreshold) {
+                    deathSound.play();
+                    isDead = true;
+                }
+
+
                 characterForwardSprite.setPosition(sf::Vector2f(characterForwardSprite.getPosition().x, 820.f));
                 velocity.y = 0;
                 isJumping = false; //reset back to static position
             }
         }
+
+        if (isDead) {
+            //logic to save score and exit gameplay
+
+            //window.close();
+        }
+
+        sf::Vector2f pos = characterForwardSprite.getPosition();
+        auto bounds = characterForwardSprite.getGlobalBounds();
+
+        // INSURE SPRITE CANT LEAVE CONSOLE
+        if (pos.x < 0) //bound x
+            pos.x = 0;
+        if (pos.x > 700 - bounds.size.x)
+            pos.x = 700 - bounds.size.x;
+
+        // bound y
+        if (pos.y < 0)
+            pos.y = 0;
+        if (pos.y > 900 - bounds.size.y)
+            pos.y = 900 - bounds.size.y;
+
+        characterForwardSprite.setPosition(pos);
 
         window.clear();
         window.draw(backgroundArtSprite);
